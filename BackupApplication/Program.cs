32046324770518a3ff380180c2d0bd97c944c6df
Haitham T. Abdulkadier (HalabjaIT) <hiethem.hiethem@yahoo.com>
2016-11-24 10:23:05 +0300

    First

---
+++ BackupApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+using Microsoft.Win32;
+using System.Threading;
+namespace BackupApplication
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+            key.SetValue(Application.ProductName, Application.ExecutablePath.ToString());
+            while (true)
+            {
+                ZipFile.CreateFromDirectory(@"d:\CurrentProjects", @"D:\Google\CurrentProjects\Backup_" + Environment.MachineName.ToString() + "_" + DateTime.Now.ToString("yyyy_MM_dd_hh_mm") + ".zip");
+                Thread.Sleep(TimeSpan.FromHours(1));
+            }
+        }
+        
+    }
+}
